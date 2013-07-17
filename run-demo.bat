f96a5aea1afd3ebd52ce016ed2a139c412c12084
Decebal Suiu <decebal.suiu@gmail.com>
2013-07-17 22:35:36 +0300

    added windows script for run demo

---
+++ run-demo.bat
@@ -0,0 +1,17 @@
+REM
+REM This script creates and run the wicket-plugin demo.
+REM
+
+REM create artifacts using maven
+call mvn clean install
+
+REM create 'plugins' folder
+cd demo/app
+rmdir plugins /s/q
+mkdir plugins
+xcopy ..\plugin1\target\wicket-plugin-demo-plugin1-*.zip plugins /s
+xcopy ..\plugin2\target\wicket-plugin-demo-plugin2-*.zip plugins /s
+
+REM run demo
+call mvn jetty:run
+cd ..\..
